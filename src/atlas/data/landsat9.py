046fabfa21c08a6f642f301925e4ada9677f320a
Angel Guillen Flores <ag@cryark.net>
2026-06-06 05:32:01 -0400

    Add Landsat 8 and 9 Clients

---
+++ src/atlas/data/landsat9.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any, ClassVar
+
+from atlas.data.base import PullRequest
+from atlas.data.planetary_computer import PlanetaryComputerClient
+
+
+class Landsat9Client(PlanetaryComputerClient):
+    default_collection: ClassVar[str] = "landsat-c2-l2"
+
+    def _build_query_filter(self, request: PullRequest) -> dict[str, Any] | None:
+        query : dict = {"platform": {"eq": "landsat-9"}}
+        if request.max_cloud_cover is None:
+            return query
+        query["eo:cloud_cover"] = {"lte": request.max_cloud_cover}
+        return query
